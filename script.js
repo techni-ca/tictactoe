b04feefece806ca8ffb1458a7e6ffde1518986ce
Andrew <48330373+techni-ca@users.noreply.github.com>
2023-03-08 16:34:06 -0700

    Refactor to remove redundent code

--- script.js
+++ script.js
@@ -1,153 +1,123 @@
 function getDiv (row, col) {
-  let elementClass = ''
-  switch (row) {
-    case 0:
-      elementClass += '.top'
-      break
-    case 1:
-      elementClass += '.center'
-      break
-    case 2:
-      elementClass += '.bottom'
-      break
-  }
-  switch (col) {
-    case 0:
-      elementClass += '.left'
-      break
-    case 1:
-      elementClass += '.middle'
-      break
-    case 2:
-      elementClass += '.right'
-      break
-  }
-  const div = document.querySelector(elementClass)
-  return div
+  return document.querySelector(
+    ['.top', '.center', '.bottom'][row] + ['.left', '.middle', '.right'][col]
+  )
 }
-function getRow (div) {
-  if (div.classList.contains('top')) return 0
-  if (div.classList.contains('center')) return 1
-  if (div.classList.contains('bottom')) return 2
+function markDisplay (row, col, mark) {
+  const div = getDiv(row, col)
+  div.textContent = mark
+  div.classList.remove('unmarked')
+  marks[row][col] = mark
 }
-function getCol (div) {
-  if (div.classList.contains('left')) return 0
-  if (div.classList.contains('middle')) return 1
-  if (div.classList.contains('right')) return 2
-}
-function markDisplay (div, xoro) {
-  div.textContent = xoro
-  div.classList.toggle('unmarked')
-}
-function playerClicked (div) {
-  if (div.classList.contains('unmarked')) {
-    markDisplay(div, 'O')
-    marked++
-    marks[getRow(div)][getCol(div)] = 'O'
-    checkForWin()
+function playerClicked (row, col) {
+  markDisplay(row, col, playerMark)
+  if (!checkForGameOver()) {
     computerPlay()
   }
 }
 function computerPlay () {
-  while (marked < 9) {
-    const row = Math.floor(Math.random() * 3)
-    const col = Math.floor(Math.random() * 3)
-    if (marks[row][col] === '') {
-      markDisplay(getDiv(row, col), 'X')
-      marked++
-      marks[row][col] = 'X'
-      checkForWin()
-      return
-    }
+  // needs better AI!
+  let row = 1
+  let col = 1
+  while (marks[row][col] !== '') {
+    row = Math.floor(Math.random() * 3)
+    col = Math.floor(Math.random() * 3)
   }
+  markDisplay(row, col, computerMark)
+  checkForGameOver()
+}
+function markWon (row, col, winner) {
+  getDiv(row, col).classList.add(`win${winner}`)
 }
 function checkSame (a, b, c) {
-  if (a === b && a === c) {
-    return a
-  }
-  return ''
+  return a === b && a === c ? a : ''
 }
 function checkRow (n) {
-  const returnValue = checkSame(marks[n][0], marks[n][1], marks[n][2])
-  if (returnValue !== '') {
+  const winner = checkSame(marks[n][0], marks[n][1], marks[n][2])
+  if (winner !== '') {
     for (let i = 0; i < 3; i++) {
-      const div = getDiv(n, i)
-      div.classList.add(`win${returnValue}`)
+      markWon(n, i, winner)
     }
   }
-  return returnValue
+  return winner
 }
 function checkCol (n) {
-  const returnValue = checkSame(marks[0][n], marks[1][n], marks[2][n])
-  if (returnValue !== '') {
+  const winner = checkSame(marks[0][n], marks[1][n], marks[2][n])
+  if (winner !== '') {
     for (let i = 0; i < 3; i++) {
-      const div = getDiv(i, n)
-      div.classList.add(`win${returnValue}`)
+      markWon(i, n, winner)
     }
   }
-  return returnValue
+  return winner
 }
-function checkD1 () {
-  const returnValue = checkSame(marks[0][0], marks[1][1], marks[2][2])
-  if (returnValue !== '') {
-    getDiv(0, 0).classList.add(`win${returnValue}`)
-    getDiv(1, 1).classList.add(`win${returnValue}`)
-    getDiv(2, 2).classList.add(`win${returnValue}`)
-  }
-  return returnValue
-}
-function checkD2 () {
-  const returnValue = checkSame(marks[0][2], marks[1][1], marks[2][0])
-  if (returnValue !== '') {
-    getDiv(0, 2).classList.add(`win${returnValue}`)
-    getDiv(1, 1).classList.add(`win${returnValue}`)
-    getDiv(2, 0).classList.add(`win${returnValue}`)
-  }
-  return returnValue
-}
-function checkForWin () {
-  let winner = ''
-  let rowwin = ''
-  let colwin = ''
-  for (let i = 0; i < 3; i++) {
-    if (rowwin === '') rowwin = checkRow(i)
-    if (colwin === '') colwin = checkCol(i)
-  }
-  const d1win = checkD1()
-  const d2win = checkD2()
-
-  winner = rowwin
-  if (winner === '') winner = colwin
-  if (winner === '') winner = d1win
-  if (winner === '') winner = d2win
-
+function checkDiagonalDown () {
+  const winner = checkSame(marks[0][0], marks[1][1], marks[2][2])
   if (winner !== '') {
-    console.log(`${winner} wins`)
-  } else if (marked === 9) {
-    console.log("cat's game")
-  } else {
-    return
+    for (let i = 0; i < 3; i++) {
+      markWon(i, i, winner)
+    }
   }
+  return winner
+}
+function checkDiagonalUp () {
+  const winner = checkSame(marks[0][2], marks[1][1], marks[2][0])
+  if (winner !== '') {
+    for (let i = 0; i < 3; i++) {
+      markWon(i, 2 - i, winner)
+    }
+  }
+  return winner
+}
+function checkForGameOver () {
+  // check for a winner
+  let winner = checkDiagonalDown() + checkDiagonalUp()
   for (let i = 0; i < 3; i++) {
-    for (let j = 0; j < 3; j++) {
-      if (marks[i][j] === '') {
-        markDisplay(getDiv(i, j), ' ')
-        marks[i][j] = ' '
+    winner += checkRow(i) + checkCol(i)
+  }
+  if (winner !== '') {
+    winner = winner[0]
+    switch (winner) {
+      case computerMark:
+        console.log('Computer Wins')
+        break
+      case playerMark:
+        console.log('Player Wins')
+        break
+    }
+    // make the board unreactive
+    for (let i = 0; i < 3; i++) {
+      for (let j = 0; j < 3; j++) {
+        if (marks[i][j] === '') {
+          markDisplay(i, j, ' ')
+        }
       }
     }
+    return true
   }
-  marked = 9
+
+  // check for full board
+  const boardFull = marks.join().length === 17
+  if (boardFull) {
+    console.log('Drawn Game')
+  }
+  return boardFull
 }
 const marks = [
   ['', '', ''],
   ['', '', ''],
   ['', '', '']
 ]
-let marked = 0
+const playerMark = 'X'
+const computerMark = 'O'
 
 if (Math.floor(Math.random() * 2) === 0) computerPlay()
 
 const board = document.querySelector('.board')
 board.addEventListener('click', e => {
-  playerClicked(e.target)
+  const classList = e.target.classList
+  if (classList.contains('unmarked')) {
+    const row = classList.contains('top') ? 0 : classList.contains('bottom') ? 2 : 1
+    const col = classList.contains('left') ? 0 : classList.contains('right') ? 2 : 1
+    playerClicked(row, col)
+  }
 })
