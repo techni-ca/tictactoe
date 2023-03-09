55772f7eebb804e98f261be6f12565adf5a5cbbd
Andrew <48330373+techni-ca@users.noreply.github.com>
2023-03-09 12:34:32 -0700

    Add scoring and improve AI

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ function markDisplay (row, col, mark) {
   const div = getDiv(row, col)
   div.textContent = mark
   div.classList.remove('unmarked')
+  div.classList.add('marked')
   marks[row][col] = mark
 }
 function playerClicked (row, col) {
@@ -15,15 +16,73 @@ function playerClicked (row, col) {
     computerPlay()
   }
 }
+function findWinningMove (mark) {
+  const winningMoves = []
+  for (let row = 0; row < 3; row++) {
+    for (let col = 0; col < 3; col++) {
+      if (marks[row][col] === '') {
+        // do all other squares on this row contain mark?
+        let winningMove = true
+        for (let i = 0; i < 3; i++) {
+          if ((i !== row) && (marks[i][col] !== mark)) {
+            winningMove = false
+          }
+        }
+        // do all other squares on this col contain mark?
+        if (winningMove === false) {
+          winningMove = true
+          for (let i = 0; i < 3; i++) {
+            if ((i !== col) && (marks[row][i] !== mark)) {
+              winningMove = false
+            }
+          }
+        }
+        // do all other squares on the down diagonal contain mark?
+        if ((winningMove === false) && (row === col)) {
+          winningMove = true
+          for (let i = 0; i < 3; i++) {
+            if ((i !== row) && (marks[i][i] !== mark)) {
+              winningMove = false
+            }
+          }
+        }
+        // do all other squares on the up diagonal contain mark?
+        if ((winningMove === false) && (row + col === 2)) {
+          winningMove = true
+          for (let i = 0; i < 3; i++) {
+            if ((i !== row) && (marks[i][2 - i] !== mark)) {
+              winningMove = false
+            }
+          }
+        }
+        if (winningMove) {
+          winningMoves.push({ row, col })
+        }
+      }
+    }
+  }
+  if (winningMoves.length === 0) {
+    return null
+  }
+  return winningMoves[Math.floor(Math.random() * winningMoves.length)]
+}
 function computerPlay () {
   // needs better AI!
-  let row = 1
-  let col = 1
-  while (marks[row][col] !== '') {
-    row = Math.floor(Math.random() * 3)
-    col = Math.floor(Math.random() * 3)
+  let nextMove = findWinningMove(computerMark)
+  if (nextMove === null) {
+    nextMove = findWinningMove(playerMark)
   }
-  markDisplay(row, col, computerMark)
+  if (nextMove === null) {
+    let row
+    let col
+    do {
+      row = Math.floor(Math.random() * 3)
+      col = Math.floor(Math.random() * 3)
+    } while (marks[row][col] !== '')
+    markDisplay(row, col, computerMark)
+  } else {
+    markDisplay(nextMove.row, nextMove.col, computerMark)
+  }
   checkForGameOver()
 }
 function markWon (row, col, winner) {
@@ -87,7 +146,8 @@ function checkForGameOver () {
     }
     winDiv.classList.remove('hide')
     winDiv.classList.add('show')
-
+    score[winner[0]]++
+    updateScores()
     // make the board unreactive
     for (let i = 0; i < 3; i++) {
       for (let j = 0; j < 3; j++) {
@@ -109,6 +169,14 @@ function checkForGameOver () {
   }
   return boardFull
 }
+function updateScores () {
+  const playerScore = document.querySelector('.player').querySelector('.score')
+  const computerScore = document
+    .querySelector('.computer')
+    .querySelector('.score')
+  playerScore.textContent = score[playerMark]
+  computerScore.textContent = score[computerMark]
+}
 const marks = [
   ['', '', ''],
   ['', '', ''],
@@ -116,15 +184,47 @@ const marks = [
 ]
 const playerMark = 'X'
 const computerMark = 'O'
-
-if (Math.floor(Math.random() * 2) === 0) computerPlay()
+const score = []
+score[playerMark] = 0
+score[computerMark] = 0
+let computerStarted = Math.floor(Math.random() * 2) === 0
+if (computerStarted) computerPlay()
 
 const board = document.querySelector('.board')
 board.addEventListener('click', e => {
   const classList = e.target.classList
   if (classList.contains('unmarked')) {
-    const row = classList.contains('top') ? 0 : classList.contains('bottom') ? 2 : 1
-    const col = classList.contains('left') ? 0 : classList.contains('right') ? 2 : 1
+    /* eslint-disable */
+    const row = classList.contains('top')
+      ? 0
+      : classList.contains('bottom')
+      ? 2
+      : 1
+    const col = classList.contains('left')
+      ? 0
+      : classList.contains('right')
+      ? 2
+      : 1
+    /* eslint-enable */
     playerClicked(row, col)
   }
+})
+const restart = document.querySelector('.restart')
+restart.addEventListener('click', e => {
+  const winDiv = document.querySelector('.winner')
+  winDiv.classList.add('hide')
+  winDiv.classList.remove('show')
+  for (let i = 0; i < 3; i++) {
+    for (let j = 0; j < 3; j++) {
+      const div = getDiv(i, j)
+      div.classList.remove('winX')
+      div.classList.remove('winO')
+      div.classList.remove('marked')
+      div.classList.add('unmarked')
+      div.textContent = ''
+      marks[i][j] = ''
+    }
+  }
+  computerStarted = !computerStarted
+  if (computerStarted) computerPlay()
 })
